013f266e1cbcbae0d3deffb00f1619a4addb43df
Nuthelk Molina <nuthelkcd@gmail.com>
2023-02-20 20:59:08 -0500

    cambios texto skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -23,7 +23,7 @@ const Skill = ({directonLeft, skill}: Props) => {
 
         <div className='absolute opacity-0 group-hover:opacity-80 transition-all duration-300 ease-out group-hover:bg-white/60 h-16 w-16 md:w-24 md:h-24 xl:w-28 xl:h-28 rounded-full z-0'>
             <div className='flex items-center text-center justify-center h-full'>
-                <p className='text-base md:text-xl font-bold text-black opacity-100'>{skill.progress}</p>
+                <p className='text-xs md:text-xl font-bold text-black opacity-100'>{skill.progress}</p>
             </div>
         </div>
 
